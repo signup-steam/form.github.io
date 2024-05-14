8cdd470188a3c6ba4119da7a241efff844c8657a
signup-steam <audrvto@gmail.com>
2024-05-14 20:00:33 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php 
 
 require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer(true);
+$mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
 $name = $_POST['user_name'];
@@ -13,12 +13,12 @@ $email = $_POST['user_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dzharuzov@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Username = 'altukhovmax2008@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Max_Russia_2008'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dzharuzov@mail.ru'); // от кого будет уходить письмо?
+$mail->setFrom('altukhovmax2008@mail.ru'); // от кого будет уходить письмо?
 $mail->addAddress('altukhovmax2008@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
@@ -28,7 +28,7 @@ $mail->addAddress('altukhovmax2008@gmail.com');     // Кому будет ух�
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с тестового сайта';
+$mail->Subject = 'Данные с сайта';
 $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
 $mail->AltBody = '';
 
